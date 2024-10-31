93151376d55247e702e5f3095f5bfcca23a32b6d
davidtavartk <d.tavartkiladze5@gmail.com>
2024-10-31 22:52:33 +0400

    bonus task121

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,7 +2,7 @@
 const Button = ({ children, className, filled }) => {
   return (
     <button
-      className={  `md:w-[400px] border-custom-pink rounded-[24px] border text-custom-pink px-[30px] py-[8px] hover:shadow-custom-pink  ${filled ? 'bg-custom-pink text-white hover:bg-[#FF8ED7]' :"" } ${className}`}
+      className={`border-custom-pink rounded-[24px] border text-custom-pink px-[30px] py-[8px] hover:shadow-custom-pink  ${filled ? 'bg-custom-pink text-white hover:bg-[#FF8ED7]' :"" } ${className}`}
     >
       {children}
     </button>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 
 const Navbar = () => {
   return (
-    <div className="w-[89%] m-auto flex justify-between mt-20">
+    <div className="w-[89%] m-auto flex justify-between mt-20 ">
       <img src={logo} alt="" />
       <Button filled={false}>Try It Free</Button>
     </div>
